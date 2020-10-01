82b0ac9385eb154081b18887e747d727ed83bc0f
gabriel0725 <gabriel.gmcontato@gmail.com>
2020-10-01 17:40:19 -0300

    nao sei

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,17 +9,21 @@ export class HomePage {
 
   public currentValue = 0;
   public maxValue = 0;
+  public selectedCash = 0;
 
   public increment(){
-    this.currentValue++
+    this.currentValue += this.selectedCash;
+    this.selectedCash = 0;
     if(this.currentValue > this.maxValue){
       this.maxValue = this.currentValue;
     }
   }
 
   public decrement(){
-    if(this.currentValue > 0){
-    this.currentValue--
+    this.currentValue -= this.selectedCash;
+    this.selectedCash = 0;
+    if(this.currentValue < 0){
+    this.currentValue = 0;
     }
   }
 
