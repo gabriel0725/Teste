0f2992f5363ba512783b3019947ce9d25450406c
Gabriel0725 <gabriel.gmcontato@gmail.com>
2020-10-07 16:37:59 -0300

    Quiz final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController,ToastController } from '@ionic/angular';
 
 type Unit = 'bit' | 'byte' | 'kilobit' | 'kilobyte' |
   'megabit' | 'megabyte' | 'gigabit' |
@@ -11,50 +12,83 @@ type Unit = 'bit' | 'byte' | 'kilobit' | 'kilobyte' |
 })
 export class HomePage {
 
-  public fromValue = 0;
-  public toValue = 0;
-  public fromUnit: Unit = 'bit';
-  public toUnit: Unit = 'byte';
-
-
-  constructor() { }
-
-  public calculate() {
-    const valueInBits = this.fromValueInBits();
-    this.toValue = this.convertToSelectedValue(valueInBits);
-  }
-
-  private fromValueInBits() {
-    return this.fromValue * this.getUnitMultiplier(this.fromUnit);
-  }
-
-  private convertToSelectedValue(bits: number) {
-    return bits / this.getUnitMultiplier(this.toUnit);
-  }
-
-  private getUnitMultiplier(unit: Unit) {
-
-    let power = 0;
-    if (unit.startsWith('kilo')) {
-      power = 1;
-    } else if (unit.startsWith('mega')) {
-      power = 2;
-    } else if (unit.startsWith('giga')) {
-      power = 3;
-    } else if (unit.startsWith('tera')) {
-      power = 4;
+  public questions = [
+    {
+      text: 'Qual seu genero favorito?',
+      options: ['RPG', 'FPS', 'Plataforma', 'MOBA'],
+      selectedOption: null,
+    },
+    {
+      text: 'Qual sua desenvolvedora favorita?',
+      options: ['Nintendo', 'EA', 'Ubisoft', 'Valve'],
+      selectedOption: null,
+    },
+    {
+      text: 'Onde voce gosta de jogar?',
+      options: ['Portatil', 'Console', 'Mobile', 'PC'],
+      selectedOption: null,
     }
-    const multiplier = unit.endsWith('byte') ? 8 : 1;
-    return multiplier * 1024 ** power;
+  ];
+
+  public currentQuestion = 0;
+  public isDone = false;
+
+  public mostVoted = null;
+  public possibleResults = [
+    {name: 'Zelda', image:'https://cdn02.nintendo-europe.com/media/images/10_share_images/portals_3/SI_Hub_Zelda_Portal_image1600w.jpg'},
+    {name: 'Battlefield', image:'https://store-images.s-microsoft.com/image/apps.62184.69271430660548627.c90235de-725c-43d6-b777-19f7f75295cc.465ea345-cd29-4a19-8c1c-4d175dc20b49?mode=scale&q=90&h=1080&w=1920'},
+    {name: 'Rayman', image:'https://uploads.jovemnerd.com.br/wp-content/uploads/2019/06/ubisoft-oferecera-rayman-origins-de-graca-saiba-como-resgatar-1210x540.jpg'},
+    {name: 'DOTA', image: 'https://www.pichauarena.com.br/wp-content/uploads/2019/05/basic.jpg'}
+  ];
+
+  constructor(
+    private toastController: ToastController,
+    private alertController: AlertController
+    ) { }
+
+  public next(){
+    if(this.currentQuestion == this.questions.length - 1){
+      this.done();
+    }else{
+      this.currentQuestion++
+      this.showToast();
+    }
   }
 
-  public change() {
-    const tempUnit = this.toUnit;
-    this.toUnit = this.fromUnit;
-    this.fromUnit = tempUnit;
+  private async showToast(){
+    const toast = await this.toastController.create({
+      header: this.currentQuestion + ' de ' + this.questions.length + ' respondidas',
+      duration: 500
+    });
+    toast.present();
+  }
 
-    const tempValue = this.toValue;
-    this.toValue = this.fromValue;
-    this.fromValue = tempValue;
+  public done(){
+    this.isDone = true;
+    const voteCount = [0, 0, 0, 0];
+    this.questions.forEach(q => voteCount[q.selectedOption]++);
+    const max = Math.max(...voteCount);
+    this.mostVoted = voteCount.findIndex(v =>  v === max);
+  }
+
+  public async confirmReset(){
+    const alert = await this.alertController.create({
+      header: 'Tem certeza?',
+      message: 'O seu resultado atual sera perdido',
+      buttons: [
+        'Cancelar',
+        {
+          text: 'Jogar de novo',
+          handler: () => this.reset()
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  private reset(){
+    this.currentQuestion = 0;
+    this.isDone = false;
+    this.questions.forEach(q => q.selectedOption = null)
   }
 }
