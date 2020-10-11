b88ddcd8f82826db555839fa81daa5c6152478d8
Gabriel0725 <gabriel.gmcontato@gmail.com>
2020-10-10 22:07:15 -0300

    Comit do jhow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'step1',
     loadChildren: () => import('./step1/step1.module').then( m => m.Step1PageModule)
+  },  {
+    path: 'step2',
+    loadChildren: () => import('./step2/step2.module').then( m => m.Step2PageModule)
   },
+
 ];
 
 @NgModule({
